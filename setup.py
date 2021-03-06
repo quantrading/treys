4a827201622692cd2422a5d17a14d6e87780d240
woo jae <dnwogo@naver.com>
2021-03-06 15:00:16 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
     name='treys',
     version='0.1.3',
     description='treys is a pure Python poker hand evaluation library',
-    long_description=open('README.rst').read(),
     author='Will Drevo',
     url='https://github.com/ihendley/treys',
     license='MIT',
